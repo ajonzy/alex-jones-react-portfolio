05f41f2423c35fb5e2ecf9eddf78f01e0c223441
alexjones <ajonzy@gmail.com>
2019-02-19 11:34:39 -0700

    Added portfolio-items and organized portfolio

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,14 @@
 import React, {Component} from "react"
 
+import PortfolioItem from "./portfolio-item"
+
 export default class extends Component {
     render() {
         return (
             <div>
                 <h2>Portfolio items go here</h2>
+
+                <PortfolioItem />
             </div>
         )
     }
