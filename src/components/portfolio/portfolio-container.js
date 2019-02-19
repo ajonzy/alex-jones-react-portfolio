042b4a0b7c2d66be20276ccd6e4b8b1148d2a923
alexjones <ajonzy@gmail.com>
2019-02-19 11:43:39 -0700

    Added constructor to portfolio-container

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,6 +3,11 @@ import React, {Component} from "react"
 import PortfolioItem from "./portfolio-item"
 
 export default class extends Component {
+    constructor() {
+        super()
+        
+        console.log("Portfolio container has rendered")
+    }
     render() {
         return (
             <div>
